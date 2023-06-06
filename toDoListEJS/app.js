488cb647c90584f5e724719d4c37dea2076eea95
la-rika <laramisiaforlani@gmail.com>
2023-06-06 18:07:48 +0200

    todolist added work to do list and global css

--- toDoListEJS/app.js
+++ toDoListEJS/app.js
@@ -3,45 +3,47 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+//settaggio per utilizzare ejs
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')) //diciamo ad express dove andare a prendere i file a cui facciamo riferimento (css, img...)
+
+let newItems = ['something to do'];
+let workItems = [];
 
 app.get("/", (req, res) => {
-
     const date = new Date(); //creo l'oggetto data di oggi
-    const today = date.getDay(); //ottendo il giorno della settimana (dom-sab) (0-6)
-    let day = "";
 
-    switch (today) {
-        case 0:
-            day = "sunday";
-            break;
-        case 1:
-            day = "monday";
-            break;
-        case 2:
-            day = "thursday";
-            break;
-        case 3:
-            day = "tuesday";
-            break;
-        case 4:
-            day = "wednesday";
-            break;
-        case 5:
-            day = "friday";
-            break;
-        case 6:
-            day = "saturday";
-            break;
-        default:
-            console.log("error: the current date is " + today)
-            break;
+    let options = {
+        weekday: 'long', //string
+        day: 'numeric',  //number 
+        month: 'long'    //string
     }
 
+    var day = date.toLocaleDateString('it-IT', options) //martedi 6 giugno
+
     //visualizzo il file list.ejs (ejs guarda sempre dentro la cartella views per i file)
-    //a questo file passo anche la variabil ekindOfDay che ha il valore di day (label: value)
-    res.render("list", { kindOfDay: day })
+    //a questo file passo anche la variabile kindOfDay che ha il valore di day (label: value)
+    //il file ejs all'intenro del quale usiamo l evariabili e' il ejs template
+    res.render("list", { title: day, newListItems: newItems }) //list: file in cui voglio usare i dati; {}: dati che voglio usare nel file html
 })
+
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    if (req.body.list === 'Work List') { //list: valore del bottone che cambia in base al titolo della pagina e quindi alla route
+        workItems.push(item);
+        res.redirect("/work"); //quando avviene la post si e' reindirizzati alla home (get)salvandosi newItem
+    } else {
+        newItems.push(item);
+        res.redirect("/"); 
+    }
+
+})
+
+app.get("/work", (req, res) => {
+    res.render("list", { title: "Work List", newListItems: workItems });
+})
+
 
 app.listen(3000, (req, res) => {
     console.log("the server is running on port 3000 !");
