a39831c039ca9290584a30da9fa9c811dbf52f31
vagrant <vagrant@dkrhost>
2023-06-09 11:07:36 +0200

    did some challenge

--- ejsChallengeBlog/app.js
+++ ejsChallengeBlog/app.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+var _ = require('lodash'); //libreria con metodi utili da usare con node
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,28 +11,28 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts =[];
+let posts = [];
 
-app.get("/", (req,res) => {
-  res.render('home', {homeContent: homeStartingContent, allPosts: posts})
+app.get("/", (req, res) => {
+  res.render('home', { homeContent: homeStartingContent, allPosts: posts })
 })
 
-app.get("/about", (req,res) => {
-  res.render('about', {aboutContent: aboutContent})
+app.get("/about", (req, res) => {
+  res.render('about', { aboutContent: aboutContent })
 })
 
-app.get("/contact", (req,res) => {
-  res.render('contact', {contactContent: contactContent})
+app.get("/contact", (req, res) => {
+  res.render('contact', { contactContent: contactContent })
 })
 
-app.get("/compose", (req,res) => {
+app.get("/compose", (req, res) => {
   res.render('compose');
 })
 
-app.post("/compose", (req,res) =>{
+app.post("/compose", (req, res) => {
   const post = {
     title: req.body.titleText,
     body: req.body.bodyText
@@ -42,6 +42,15 @@ app.post("/compose", (req,res) =>{
   res.redirect("/");
 })
 
-app.listen(3000, function() {
+app.get("/posts/:postName", (req, res) => {
+  posts.forEach(function (post) {
+     //metodo lowercase ritorna una stringa e ignora lettere grandi e segni speciali
+    if (_.lowerCase(req.params.postName) === _.lowerCase(post.title)) {
+      res.render('post', {postTitle: post.title, postContent: post.body})
+    }
+  })
+})
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
