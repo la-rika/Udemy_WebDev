69986d44fe32aeec48024c6276a5f1a244e5394c
Lara Misia Forlani <larika@Rosie.home>
2023-09-05 21:41:17 +0200

    started implementing mongodb in todolist app

--- toDoListEJS/app.js
+++ toDoListEJS/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const getDay = require(__dirname + "/day.js")
+const mongoose = require('mongoose')
 
 const app = express();
 
@@ -9,15 +10,46 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public')) //diciamo ad express dove andare a prendere i file a cui facciamo riferimento (css, img...)
 
-const newItems = ['something to do'];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+})
+
+const Item = mongoose.model('item', itemsSchema)
+
+const item1 = new Item({
+    name: 'be happy'
+})
+
+const item2 = new Item({
+    name: 'dont tilt'
+})
+
+const item3 = new Item({
+    name: 'love yourself and your company'
+})
+
+const defaultItems = [item1,item2,item3]
+
 
 app.get("/", (req, res) => {
-    const day = getDay();
     //visualizzo il file list.ejs (ejs guarda sempre dentro la cartella views per i file)
     //a questo file passo anche la variabile kindOfDay che ha il valore di day (label: value)
     //il file ejs all'intenro del quale usiamo l evariabili e' il ejs template
-    res.render("list", { title: day, newListItems: newItems }) //list: file in cui voglio usare i dati; {}: dati che voglio usare nel file html
+    Item.find().then(function(items){
+        if(items.length===0){
+            Item.insertMany(defaultItems).then(function(){
+                console.log('items successfully saved')
+            }).catch(function(err){
+                console.log(err)
+            })
+        }else{
+            res.render("list", { title: "today", newListItems: items }) //list: file in cui voglio usare i dati; {}: dati che voglio usare nel file html
+        }
+    }).catch(function(err){
+        console.log(err)
+    })
 })
 
 app.post("/", (req, res) => {
