306bf8d17fcebf016e8060673736378b6ea402e4
la-rika <laramisiaforlani@gmail.com>
2023-10-02 18:23:16 +0200

    Revert "finished todolist app"
    This reverts commit 03b83e9a800e2ade5a3e2812437a4df20217f8c9.

---
+++ toDoListEJS/app.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const getDay = require(__dirname + "/day.js")
+const mongoose = require('mongoose')
+
+const app = express();
+
+//settaggio per utilizzare ejs
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')) //diciamo ad express dove andare a prendere i file a cui facciamo riferimento (css, img...)
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+})
+
+const Item = mongoose.model('item', itemsSchema)
+
+const item1 = new Item({
+    name: 'be happy'
+})
+
+const item2 = new Item({
+    name: 'dont tilt'
+})
+
+const item3 = new Item({
+    name: 'love yourself and your company'
+})
+
+const defaultItems = [item1, item2, item3]
+
+const listSchema = new mongoose.Schema({
+    name: String,
+    items: [itemsSchema]
+})
+
+const List = mongoose.model("List", listSchema)
+
+
+app.get("/", (req, res) => {
+    //visualizzo il file list.ejs (ejs guarda sempre dentro la cartella views per i file)
+    //a questo file passo anche la variabile kindOfDay che ha il valore di day (label: value)
+    //il file ejs all'intenro del quale usiamo l evariabili e' il ejs template
+    Item.find().then((items) => {
+        if (items.length === 0) {
+            Item.insertMany(defaultItems).then(() => {
+                console.log('items successfully saved')
+            }).catch((err) => {
+                console.log(err)
+            })
+        } else {
+            res.render("list", { title: "today", newListItems: items }) //list: file in cui voglio usare i dati; {}: dati che voglio usare nel file html
+        }
+    }).catch((err) => {
+        console.log(err)
+    })
+})
+
+app.post("/", (req, res) => {
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName
+    })
+    item.save();
+    res.redirect('/')
+})
+
+app.post('/delete', (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove({ _id: String(checkedItemId) }).then(function () {
+        console.log("Data deleted"); // Success
+        res.redirect("/")
+    }).catch(function (error) {
+        console.log(error); // Failure
+    });
+})
+
+//in base a cosa metto nel url dopo '/' vado in una nuova pagina
+//dynamic routing
+app.get('/:customList', (req, res) => {
+    const customList = req.params.customList
+
+    List.findOne({ name: customList }).then((result) => {
+        if (result) { //result diverso da undefined
+            res.render("list", { title: result.name, newListItems: result.items }) //list: file in cui voglio usare i dati; {}: dati che voglio usare nel file html
+        } else {
+
+            const list = new List({
+                name: customList,
+                items: defaultItems
+            })
+
+            list.save();
+            setTimeout(() => { res.redirect('/' + customList);}, 2000);
+        }
+    })
+
+})
+
+
+app.listen(3000, (req, res) => {
+    console.log("the server is running on port 3000 !");
+})
